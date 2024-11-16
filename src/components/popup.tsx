fd0431f0ccd9c81877eb4d0d9be15268e57216cd
iAmShivay <shivaysharma77894@gmail.com>
2024-11-16 12:22:59 +0530

    feat: add WelcomePopup component to display a welcome message on app launch

---
+++ src/components/popup.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import invite from "../assets/images/Invite.png";
+
+const WelcomePopup = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    // Show popup when component mounts
+    setOpen(true);
+  }, []);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      maxWidth="md"
+      fullWidth
+      classes={{
+        paper: "rounded-lg",
+      }}
+    >
+      <div className="relative">
+        <DialogTitle className="flex justify-between items-center pb-2">
+          {/* <span className="text-xl font-semibold">Welcome!</span> */}
+          <IconButton
+            onClick={handleClose}
+            className="absolute top-2 right-2"
+            size="small"
+          >
+            <CloseIcon className="h-5 w-5" />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent className="px-6 pb-6">
+          <div className="flex flex-col items-center space-y-4">
+            <div className="w-full flex justify-center">
+              <img
+                src={invite}
+                alt="Welcome"
+                className="max-w-full h-auto max-h-[60vh] object-contain rounded-lg shadow-md"
+              />
+            </div>
+          </div>
+        </DialogContent>
+      </div>
+    </Dialog>
+  );
+};
+
+export default WelcomePopup;
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "../src/store.tsx";
 import { Toaster } from "react-hot-toast";
-import './css/global.css';
+import "./css/global.css";
+import WelcomePopup from "./components/popup.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <Toaster position="top-center" reverseOrder={false} />
+    <WelcomePopup />
     <App />
   </Provider>
 );
